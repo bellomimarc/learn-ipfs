b74319f9b7605383baf51742ec2231943e8e0277
Marcello Bellomi <marcello.bellomi@corvina.io>
2024-01-16 15:54:16 +0100

    upload file to IPFS

---
+++ pinFileToIPFS.js
@@ -0,0 +1,42 @@
+const axios = require('axios')
+const FormData = require('form-data')
+const fs = require('fs')
+const JWT = process.env.PINATA_API_KEY
+
+if (!JWT) {
+  console.error('MISSING PINATA API KEY')
+  process.exit(1)
+}
+
+const pinFileToIPFS = async () => {
+    const formData = new FormData();
+    const src = "./data/something.json";
+    
+    const file = fs.createReadStream(src)
+    formData.append('file', file)
+    
+    const pinataMetadata = JSON.stringify({
+      name: 'Sample html',
+    });
+    formData.append('pinataMetadata', pinataMetadata);
+    
+    const pinataOptions = JSON.stringify({
+      cidVersion: 0,
+    })
+    formData.append('pinataOptions', pinataOptions);
+
+    try{
+      const res = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
+        maxBodyLength: "Infinity",
+        headers: {
+          'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+          'Authorization': `Bearer ${JWT}`
+        }
+      });
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+      console.log(error?.response?.data);
+    }
+}
+pinFileToIPFS()
